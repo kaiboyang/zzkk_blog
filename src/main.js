28b7b1bcc6f931ab57ac1ca63fd5729b55412c8b
杨凯博 <1831255794@qq.com>
2021-01-07 16:47:21 +0800

    博客最终版

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './plugins/ant-design-vue.js'
 import './components/bubble/JSClickBubble.user'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://10.0.117.64:8080/Blog_war_exploded'
+axios.defaults.baseURL = 'http://10.0.117.38:8080/Blog_war_exploded'
 axios.defaults.timeout = 5000
 
 Vue.config.productionTip = false
